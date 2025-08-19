0511e4ac3b805499e8f3345e73c1f2f99d52af83
trainingsrepo <trainings.anil@gmail.com>
2025-08-19 13:13:34 +0530

    feature102 changes by ethan

--- Application.java
+++ Application.java
@@ -21,5 +21,26 @@ PrintWriter out;
 
         startReading();
         startWriting();
+#feature102 code changes by ethan on usermodule on october 2024
+ private String formatNumber(int value) {
+        return String.format("%d", value);
+    }
 
+    private String formatNumber(double value) {
+        return String.format("%.3f", value);
+    }
+
+    private String formatNumber(String value) {
+        return String.format("%.2f", Double.parseDouble(value));
+    }
+
+    public static void main(String[] args) {
+        HelperService hs = new HelperService();
+        System.out.println(hs.formatNumber(500));
+        System.out.println(hs.formatNumber(89.9934));
+        System.out.println(hs.formatNumber("550"));
+    }
+}
+
+#end of feature102
  } catch(Exception e ){};}
